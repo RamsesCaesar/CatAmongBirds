5538a5c7b9dd9cd4a504a1bb7a424842ffcc9f7b
RamsesCaesar <66065017+RamsesCaesar@users.noreply.github.com>
2021-08-09 11:24:35 -0400

    Added collectible hearts (WIP)

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
+onready var heart = preload("res://Collectibles/Heart.tscn")
+
 const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
+var randomizer
+
+func _ready():
+	randomizer = RandomNumberGenerator.new()
 
 func create_grass_effect():
 	var grassEffect = GrassEffect.instance()
@@ -9,4 +16,10 @@ func create_grass_effect():
 
 func _on_HurtBox_area_entered(area):
 	create_grass_effect()
+	randomizer.randomize()
+	var randomNumber = randomizer.randi_range(0,6)
+	if randomNumber == 0:
+		var newHeart = heart.instance()
+		newHeart.global_position = global_position
+		get_tree().current_scene.add_child(newHeart)
 	queue_free()
