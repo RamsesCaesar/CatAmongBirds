60eb48fe2083214d38b178465af54357acbcbfc4
RamsesCaesar <66065017+RamsesCaesar@users.noreply.github.com>
2022-03-16 14:29:45 +0100

    Added heart pickup sound
    When the cat picks up a heart, it now plays a sound.

--- Collectibles/Heart.gd
+++ Collectibles/Heart.gd
@@ -19,4 +19,5 @@ func _ready():
 func _on_Area2D_area_entered(area):
 	if area.name == "ItemSnatcher" and stats.health < stats.max_health:
 		stats.set_health(stats.health + 1)
+		get_tree().current_scene.get_node("WorldSounds").play()
 		queue_free()
