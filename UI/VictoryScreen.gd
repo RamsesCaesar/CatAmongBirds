79fd59e34d9c2cb232a5f86e9eec648e3b620bcc
Paul Heinrich Bayer <66065017+RamsesCaesar@users.noreply.github.com>
2022-05-25 18:44:03 +0200

    End score (coins)
    At victory, the number of coins is displayed.

--- UI/VictoryScreen.gd
+++ UI/VictoryScreen.gd
@@ -11,3 +11,4 @@ func _on_VictoryScreen_visibility_changed():
 	emit_signal("stop_music")
 	timer.start(0.5)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if self.visible else Input.MOUSE_MODE_HIDDEN)
+	$ColorRect/YouDiedLabel.text += "\nCoins:" + str(PlayerStats.coins) 
