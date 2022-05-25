3f147f6645ed693dc82deaa7e9a6ce27d629d48a
Paul Heinrich Bayer <66065017+RamsesCaesar@users.noreply.github.com>
2022-05-25 22:11:40 +0200

    Minor fix: Bats to birds

--- UI/RestartButton.gd
+++ UI/RestartButton.gd
@@ -1,7 +1,7 @@
 extends TextureButton
 
 func _pressed():
-	PlayerStats.set_bats(0)
+	PlayerStats.set_birds(0)
 	PlayerStats.set_health(4)
 	get_tree().set_deferred("paused", false)
 	get_tree().reload_current_scene()
