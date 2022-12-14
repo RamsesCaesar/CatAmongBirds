0c4e33c2b2c101f6881374c3c8ca6293bad32db5
Paul Heinrich Bayer <66065017+RamsesCaesar@users.noreply.github.com>
2022-12-14 21:51:58 +0100

    Turkey.tscn: Walk and Stand animation (prototypes)
    Added rudimentary animations for a walking and a standing state of the new turkey.

--- Enemies/Turkey/Turkey.gd
+++ Enemies/Turkey/Turkey.gd
@@ -10,7 +10,11 @@ extends "res://Enemies/Bird.gd"
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	match state: 
+		IDLE:
+			$AnimatedSprite.set("animation", "Standing")
+		WANDER:
+			$AnimatedSprite.set("animation", "Walking")
+		CHASE:
+			$AnimatedSprite.set("animation", "Walking")
