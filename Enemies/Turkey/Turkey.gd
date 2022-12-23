aecf9d41a27f5684b24a17c0ea359784131b3fab
RamsesCaesar <66065017+RamsesCaesar@users.noreply.github.com>
2022-12-23 19:54:15 +0100

    Cleaned up code in Turkey.gd

--- Enemies/Turkey/Turkey.gd
+++ Enemies/Turkey/Turkey.gd
@@ -1,15 +1,5 @@
 extends "res://Enemies/Bird.gd"
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _physics_process(delta):
 	if playerDetectionZone.can_see_player():
 		$AnimatedSprite.animation = "Angry"
