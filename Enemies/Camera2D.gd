4974f7b72fd0c0727ffc2034c49d82ec7c0396cf
Paul Heinrich Bayer <66065017+RamsesCaesar@users.noreply.github.com>
2023-03-02 20:19:29 +0100

    Added Camera functionality to Level Template
    The camera now follows the player around in the level template, which new levels will inherit. I had to look up the camera movement logic in HeartBeast's tutorial. Also, I had to move the Camera's child nodes, TopLeft and BottomRight, in order for the camera to be able to move.

--- Enemies/Camera2D.gd
+++ Enemies/Camera2D.gd
@@ -1,5 +1,12 @@
 extends Camera2D
 
+# In Cat Among Birds, the camera gets moved around by the Player's child node,
+# called RemoteTransform2D. There is no GDScript code for that movement.
+# As a resource, compare HeartBeasts tutorial :
+# https://youtu.be/MA7jSbXfS34
+# (RamsesCaesar 2023-03-02 20:15)
+
+
 onready var topLeft = $Limits/TopLeft
 onready var bottomRight = $Limits/BottomRight
 
