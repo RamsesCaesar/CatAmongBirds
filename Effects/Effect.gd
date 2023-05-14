01e95f6ad1520e5f3ba76c2706611e10075c70ba
RamsesCaesar <66065017+RamsesCaesar@users.noreply.github.com>
2023-05-14 14:01:33 +0200

    New hit effect (refactoring)
    The game was using HeartBeast's hit effect so far. I'm replacing it with a similar, original artwork in order to achieve the goal of having it all be original assets.

--- Effects/Effect.gd
+++ Effects/Effect.gd
@@ -3,7 +3,10 @@ extends AnimatedSprite
 func _ready():
 	self.connect("animation_finished", self, "_on_animation_finished")
 	frame = 0
-	play("Animate")
+	#play("Animate")
+	# Testing making my own effect animation:
+	# (RamsesCaesar, 2023-05-03 22:20)
+	play("Prototype")
 
 func _on_animation_finished():
 	queue_free()
