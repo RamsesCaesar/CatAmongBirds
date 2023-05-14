7ed71b8086b2ddf27d81729b7f2c2fe6e3755698
Paul Heinrich Bayer <66065017+RamsesCaesar@users.noreply.github.com>
2023-05-14 17:17:50 +0200

    Set main menu animation to start only once game starts

---
+++ UI/Menus and Pop Ups/MainMenu/ForegroundElements.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$PlayerImage.playing = true
+	$BirdImage.playing = true
